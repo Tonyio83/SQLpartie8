d06c6b0462f682bfa0eebf629fb0c2bbeee57085
Tonyio83 <anthonyfrancelle@gmail.com>
2020-01-23 09:07:30 +0100

    Partie 8 correction

--- data.sql
+++ data.sql
@@ -6,18 +6,18 @@ ON languages.id = `languagesId`;
 -- exercice 2 : Afficher tous les frameworks associés à leurs langages. Si un langage n'a pas de framework ne pas l'afficher.
 SELECT languages.name AS `language`, frameworks.name AS `frameworks`
 FROM `languages`
-RIGHT JOIN `frameworks`
+INNER JOIN `frameworks`
 ON languages.id = `languagesId`;
 -- exercice 3 : Afficher le nombre de framework qu'a un langage.
-SELECT languages.name AS `language`, COUNT(frameworks.languagesId) AS `number_framework`
+SELECT languages.name AS `language`, COUNT(frameworks.Id) AS `number_framework`
 FROM `languages`
 LEFT JOIN `frameworks`
 ON languages.id = `languagesId`
 GROUP BY languages.name;
 -- exercice 4 : Afficher les langages ayant plus de 3 frameworks.
-SELECT languages.name AS `language`
-FROM `languages`
-RIGHT JOIN `frameworks`
-ON languages.id = `languagesId`
-GROUP BY languages.name
-HAVING COUNT(frameworks.languagesId) > 3;
+SELECT l.name AS `language`
+FROM `languages` AS `l`
+RIGHT JOIN `frameworks` AS `f`
+ON l.id = f.`languagesId`
+GROUP BY l.name
+HAVING COUNT(f.id) > 3;
